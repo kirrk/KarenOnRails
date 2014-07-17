41d7270ba677d3adb45e264b0e1054186d42ca1c
Karen Kirr <kkirr77@yahoo.com>
2014-07-17 12:43:28 -0700

    update files

---
+++ app/controllers/skills_controller.rb
@@ -0,0 +1,14 @@
+class SkillsController < ApplicationController
+  def index
+  end
+
+ #   def show
+ #     respond_to do |format|
+
+ #   format.html # show.html.erb
+ #   format.json { render 'skills.json' }
+
+ # end
+ # end
+
+end
