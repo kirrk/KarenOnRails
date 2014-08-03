780a2abbef330bb064c0bc7566c85475542bc5d7
Karen Kirr <kkirr77@yahoo.com>
2014-08-03 15:04:14 -0700

    update contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < ActiveRecord::Base
   validates :name, :presence => true
-  validates :subject, :presence => true,
-                      :length => { :minimum => 10 }
+  validates :subject, :presence => true
   validates :email, :presence => true
-  validates :content, :presence => true
+  validates :content, :presence => true,
+                      :length => { :minimum => 20, too_short: "inquiry is too short"}
 end
