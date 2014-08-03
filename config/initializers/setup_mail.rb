bb58821fc92cfe4de9f5b6caa0c80d66be22d5ba
Karen Kirr <kkirr77@yahoo.com>
2014-08-02 21:05:19 -0700

    update mailer

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,9 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :address => "smtp.gmail.com",
   :port => 587,
-  :user_name => ENV["EMAIL"],
-  :password => ENV["PWD"],
+  :user_name => "kkirr77@gmail.com",
+  :password => "karendesign99",
   :authentication => "plain",
+  :enable_starttls_auto => true
 
 }
