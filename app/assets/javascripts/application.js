c9eaff1eb9baaa8da44221ef6a249b8a73877efe
Karen Kirr <kkirr77@yahoo.com>
2014-11-16 12:42:06 -0800

    update scripts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -84,7 +84,7 @@ $('div.social.dash a').find('img').hover(function() {
         $(this).attr("src", "/assets/kk-pic-1.jpg").stop().fadeTo(1000, 1);
   });
 
-
+      $('div.left-home').delay(700).fadeIn(1000);
 
 });
 
