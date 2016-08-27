45d8271e0d2f810923319302edec4745b151a231
Karen Kirr <kkirr77@yahoo.com>
2016-08-27 14:16:20 -0700

    update creds

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :address => "smtp.gmail.com",
   :port => 587,
   :user_name => "kkirr77@gmail.com",
-  :password => "karendesign99",
+  :password => "karendesign77",
   :authentication => "plain",
   :enable_starttls_auto => true
 
